7325e5ed6d7e2342abe5f9d49f16cefec05dcf6f
ahy9ng <ahy9ng@virginia.edu>
2015-03-11 22:28:48 -0400

    grid

--- app/src/main/java/intelligence/geniusapp/GameModes.java
+++ app/src/main/java/intelligence/geniusapp/GameModes.java
@@ -4,14 +4,157 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 
 public class GameModes extends ActionBarActivity {
+/*    GridView gridView;
 
+    static final String[] modes = new String[] {
+
+            "흑과 백 I", "흑과 백 II", "전략윷놀이", "인디언 포커", "인디언 홀덤"
+    };*/
+    TextView t1, t2, t3, t4, t5, t6, t7, t8, t9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_modes);
+        /*gridView = (GridView) findViewById(R.id.gridview1);
+
+        // Create adapter to set value for grid view
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, modes);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText()  , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+*/
+        t1 = (TextView) findViewById(R.id.text1);
+        t2 = (TextView) findViewById(R.id.text2);
+        t3 = (TextView) findViewById(R.id.text3);
+        t4 = (TextView) findViewById(R.id.text4);
+        t5 = (TextView) findViewById(R.id.text5);
+        t6 = (TextView) findViewById(R.id.text6);
+        t7 = (TextView) findViewById(R.id.text7);
+        t8 = (TextView) findViewById(R.id.text8);
+        t9 = (TextView) findViewById(R.id.text9);
+
+        t1.setClickable(true);
+        t2.setClickable(true);
+        t3.setClickable(true);
+        t4.setClickable(true);
+        t5.setClickable(true);
+        t6.setClickable(true);
+        t7.setClickable(true);
+        t8.setClickable(true);
+        t9.setClickable(true);
+
+        t1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[0,0] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[0,1] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[0,2] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t4.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[1,0] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t5.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[1,1] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t6.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[1,2] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t7.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[2,0] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t8.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[2,1] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t9.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "[2,2] Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
